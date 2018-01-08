1f70e99e6cc9c17deb7b3bad79dc382e6ffa31b7
Takahito Iwase <hito.0525@gmail.com>
2018-01-08 21:19:09 +0900

    update routes in topics using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'topics/index'
+  resources :topics
+  # get 'topics/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
