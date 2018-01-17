39fd69d4d57e7381b92884a1f413ae4f05a81d3f
Takahito Iwase <hito.0525@gmail.com>
2018-01-18 00:31:08 +0900

    add order method in topic index using reverse order

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,7 @@
 class TopicsController < ApplicationController
   def index
-    @topics =  Topic.all
+    @topics = Topic.all
+    @topics = Topic.order(:created_at).reverse_order
   end
 
   def new
