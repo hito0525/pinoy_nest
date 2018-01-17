dcb0086634f4e776d0d20cb2583b9004b31b524b
Takahito Iwase <hito.0525@gmail.com>
2018-01-17 21:47:14 +0900

    create including updating new and create function.and defined topics params

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,9 +4,17 @@ class TopicsController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
   end
 
   def create
+    Topic.create(topic_params)
+    redirect_to topics_path
+  end
+
+private
+  def topic_params
+    params.require(:topic).permit(:title,:content)
   end
 
 end
