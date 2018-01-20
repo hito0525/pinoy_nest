58528a5ff2edbba025b48774ad2bdd4e7ec635bf
Takahito Iwase <hito.0525@gmail.com>
2018-01-20 20:09:31 +0900

    creating contact controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :topics, only:[:index,:new,:create,:edit,:update, :destroy]
-  # get 'topics/index'
-
-   get "/" => "topics#index"
+  get "/" => "topics#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# get 'topics/index'
+# get 'contacts/new'
