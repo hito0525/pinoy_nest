64d1f233f62b95bea782e7e837cd682f6e7898a8
Takahito Iwase <hito.0525@gmail.com>
2018-02-09 13:42:05 +0900

    editing contacts new and create function

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,16 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+  def create
+    Contact.create(contacts_params)
+    redirect_to new_contact_path
+  end
+
+  private
+  def contacts_params
+    params.require(:contact).permit(:name,:email,:content)
+  end
+
 end
+
