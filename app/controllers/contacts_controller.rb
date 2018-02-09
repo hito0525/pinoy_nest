1605ddb653aff304ee192e51859f23fbbf76599c
Takahito Iwase <hito.0525@gmail.com>
2018-02-09 14:20:08 +0900

    setting notice option

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,24 @@
 class ContactsController < ApplicationController
   def new
+    if params[:back]
+    @contact = Contact.new(contact_params)
+  else
     @contact = Contact.new
   end
+end
   def create
-    Contact.create(contacts_params)
-    redirect_to new_contact_path
+    @contact = Contact.new(contacts_params)
+    if @contact.save
+    # Contact.create(contacts_params)
+    redirect_to new_contact_path,notice:"Successfully Done.Thank you for contacting us"
+  else
+    render :new
+    end
   end
 
   private
   def contacts_params
     params.require(:contact).permit(:name,:email,:content)
   end
-
 end
 
